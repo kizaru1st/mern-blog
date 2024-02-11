14e02c5f0915652f5d7439caa2cc6f58b661cd65
MUHAMMAD REZA RIZKI RAHMADI <rezarizki77@gmail.com>
2024-02-11 19:39:11 +0700

    First Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <React.Fragment>
+        <h1 className='text-3xl font-bold text-red-500'>app</h1>
+    </React.Fragment>
+  )
+}
